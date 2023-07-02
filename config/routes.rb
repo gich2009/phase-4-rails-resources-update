c4cc3a4e1e9f0e1afeffc7e93781473da72d1e2c
gich2009 <waneneamos@gmail.com>
2023-07-03 00:09:42 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :birds, only: [:index, :show, :create, :update]
   
+  #The route for updating likes is:
+  #PATCH /birds/likes/:id
+  #The body should be empty.
   namespace :birds do
     resources :likes, only: :update
   end
